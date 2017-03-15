e2c1722b594be6612b49968ab3e8aa8f526dbd3a
Mudada <mudada@mudadas-mbp.home>
2017-03-15 22:21:08 +0100

    #26 done

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'F1 Gentleman Bet',
+    'name' => 'F1 Gentleman\'s Bet',
 
     /*
     |--------------------------------------------------------------------------
